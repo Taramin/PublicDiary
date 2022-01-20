06d5f01cd4db7aff7a767421c83c7a1b73aa0106
d <dtaramin@mail.ru>
2022-01-20 10:12:54 -0600

    Refactoring

--- src/main/java/com/taramin/testProject/controller/WebController.java
+++ src/main/java/com/taramin/testProject/controller/WebController.java
@@ -2,10 +2,7 @@ package com.taramin.testProject.controller;
 
 import com.taramin.testProject.entity.CurrentUser;
 import com.taramin.testProject.entity.User;
-import com.taramin.testProject.repository.PostLikeRepository;
-import com.taramin.testProject.repository.PostRepository;
 import com.taramin.testProject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
